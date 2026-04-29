5f9638de432adb2d5871a62d6812a5ec2dcc06f1
jamesmoore01 <jamesmoor301@gmail.com>
2026-04-29 12:59:04 -0700

    Update GolemListener.java

--- src/main/java/com/golemenhancer/GolemListener.java
+++ src/main/java/com/golemenhancer/GolemListener.java
@@ -14,6 +14,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
